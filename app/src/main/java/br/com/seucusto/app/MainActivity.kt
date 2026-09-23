1bda8cf183bd27d484dc551483b241fbfa0fa827
andstyle <andermuchael@gmail.com>
2026-09-22 21:49:37 -0300

    feat: inject Room repository into app navigation

--- app/src/main/java/br/com/seucusto/app/MainActivity.kt
+++ app/src/main/java/br/com/seucusto/app/MainActivity.kt
@@ -2,6 +2,14 @@ package br.com.seucusto.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import br.com.seucusto.app.data.DatabaseProvider
+import br.com.seucusto.app.data.SeucustoRepository
 import br.com.seucusto.app.ui.SeucustoApp
 import br.com.seucusto.app.ui.theme.SeucustoTheme
-class MainActivity:ComponentActivity(){override fun onCreate(savedInstanceState:Bundle?){super.onCreate(savedInstanceState);setContent{SeucustoTheme{SeucustoApp()}}}}
+class MainActivity:ComponentActivity(){
+ override fun onCreate(savedInstanceState:Bundle?){
+  super.onCreate(savedInstanceState)
+  val repo=SeucustoRepository(DatabaseProvider.get(this))
+  setContent{SeucustoTheme{SeucustoApp(repo)}}
+ }
+}
