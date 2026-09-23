fbf63c3b7b8d7b9129c52de084964c421612e981
andstyle <andermuchael@gmail.com>
2026-09-22 22:01:33 -0300

    fix: expose product component dao

--- app/src/main/java/br/com/seucusto/app/data/SeucustoDatabase.kt
+++ app/src/main/java/br/com/seucusto/app/data/SeucustoDatabase.kt
@@ -2,4 +2,9 @@ package br.com.seucusto.app.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 @Database(entities=[ProductEntity::class,RawMaterialEntity::class,ProductComponentEntity::class,StockEntity::class,StockMovementEntity::class],version=1,exportSchema=true)
-abstract class SeucustoDatabase:RoomDatabase(){abstract fun productDao():ProductDao;abstract fun rawMaterialDao():RawMaterialDao;abstract fun stockDao():StockDao}
+abstract class SeucustoDatabase:RoomDatabase(){
+ abstract fun productDao():ProductDao
+ abstract fun rawMaterialDao():RawMaterialDao
+ abstract fun productComponentDao():ProductComponentDao
+ abstract fun stockDao():StockDao
+}
