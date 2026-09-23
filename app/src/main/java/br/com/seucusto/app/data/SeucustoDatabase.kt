2b2a7d2bb36b026d928667cc537d55392918ea78
andstyle <andermuchael@gmail.com>
2026-09-22 22:09:12 -0300

    chore: avoid Room schema export warning in debug build

--- app/src/main/java/br/com/seucusto/app/data/SeucustoDatabase.kt
+++ app/src/main/java/br/com/seucusto/app/data/SeucustoDatabase.kt
@@ -1,7 +1,7 @@
 package br.com.seucusto.app.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
-@Database(entities=[ProductEntity::class,RawMaterialEntity::class,ProductComponentEntity::class,StockEntity::class,StockMovementEntity::class],version=1,exportSchema=true)
+@Database(entities=[ProductEntity::class,RawMaterialEntity::class,ProductComponentEntity::class,StockEntity::class,StockMovementEntity::class],version=1,exportSchema=false)
 abstract class SeucustoDatabase:RoomDatabase(){
  abstract fun productDao():ProductDao
  abstract fun rawMaterialDao():RawMaterialDao
