08ea7a87a773268e56e6cf86da1ef3fabab247f2
andstyle <andermuchael@gmail.com>
2026-09-22 21:44:28 -0300

    refactor: stabilize Room database provider

--- app/src/main/java/br/com/seucusto/app/data/DatabaseProvider.kt
+++ app/src/main/java/br/com/seucusto/app/data/DatabaseProvider.kt
@@ -1,4 +1,7 @@
 package br.com.seucusto.app.data
 import android.content.Context
 import androidx.room.Room
-object DatabaseProvider{ @Volatile private var instance:SeucustoDatabase?=null; fun get(context:Context)=instance?:synchronized(this){instance?:Room.databaseBuilder(context.applicationContext,SeucustoDatabase::class.java,"seucusto.db").build().also{instance=it}}}
+object DatabaseProvider{
+ @Volatile private var instance:SeucustoDatabase?=null
+ fun get(context:Context):SeucustoDatabase=instance?:synchronized(this){instance?:Room.databaseBuilder(context.applicationContext,SeucustoDatabase::class.java,"seucusto.db").build().also{instance=it}}
+}
